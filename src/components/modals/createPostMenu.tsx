3abdf5e8fb903e54cc524be6bd11c3372230fbce
Babatunde Adebowale <babatundeadebowale@AFEXs-MacBook-Pro-2.local>
2023-07-02 08:47:10 +0100

    update

--- src/components/modals/createPostMenu.tsx
+++ src/components/modals/createPostMenu.tsx
@@ -5,42 +5,51 @@ import { showNotification } from "@mantine/notifications";
 import { createPost } from "@/actions/createPost";
 import { useQueryClient } from "@tanstack/react-query";
 
-function CreatePostMenu({ opened, closeModal, setLoading, audio, recordedAudio, selected, form, setOpened }) {
-    const queryClient = useQueryClient()
+function CreatePostMenu({
+  opened,
+  closeModal,
+  setLoading,
+  audio,
+  recordedAudio,
+  selected,
+  form,
+  setOpened,
+}) {
+  const queryClient = useQueryClient();
 
-    const savePostOrDraft = (publish: "True" | "False") => {
-        if (selected.filter((item) => item.type === "image").length > 5)
-          return showNotification({
-            title: "Error",
-            message: "You cannot create post with more than 5 photos",
-            color: "red",
-          });
-        var data = new FormData();
-        data.append("text", form.values.text);
-        if (audio) {
-          data.append("audio", audio, audio.name);
-        } else if (recordedAudio) {
-          data.append("audio", recordedAudio?.blob);
+  const savePostOrDraft = (publish: "True" | "False") => {
+    if (selected.filter((item) => item.type === "image").length > 5)
+      return showNotification({
+        title: "Error",
+        message: "You cannot create post with more than 5 photos",
+        color: "red",
+      });
+    var data = new FormData();
+    data.append("text", form.values.text);
+    if (audio) {
+      data.append("audio", audio, audio.name);
+    } else if (recordedAudio) {
+      data.append("audio", recordedAudio?.blob);
+    }
+    selected.length &&
+      selected.forEach((item) => {
+        if (item.type === "image") {
+          if (typeof item.value === "string") {
+            data.append("photo", item.value);
+          } else data.append("photo", item.value, item.value.name);
+        } else if (item.type === "video") {
+          if (typeof item.value === "string") {
+            data.append("video", item.value);
+          } else data.append("video", item.value, item.value.name);
         }
-        selected.length &&
-          selected.forEach((item) => {
-            if (item.type === "image") {
-              if (typeof item.value === "string") {
-                data.append("photo", item.value);
-              } else data.append("photo", item.value, item.value.name);
-            } else if (item.type === "video") {
-              if (typeof item.value === "string") {
-                data.append("video", item.value);
-              } else data.append("video", item.value, item.value.name);
-            }
-          });
-        data.append("is_article", "yes");
-        data.append("publish", publish);
-        createPost(data, publish, setLoading, () => {
-          queryClient.invalidateQueries(["all-posts"]);
-          closeModal();
-        });
-      };
+      });
+    data.append("is_article", "yes");
+    data.append("publish", publish);
+    createPost(data, publish, setLoading, () => {
+      queryClient.invalidateQueries(["all-posts"]);
+      closeModal();
+    });
+  };
 
   return (
     <Menu
@@ -68,41 +77,40 @@ function CreatePostMenu({ opened, closeModal, setLoading, audio, recordedAudio, 
       }}
     >
       <Menu.Target>
-        <PrimaryButton
-          text="Share"
-          onClick={() => {}}
-        />
+        <div className="relative">
+          <PrimaryButton text="Share" onClick={() => {}} />
+        </div>
       </Menu.Target>
 
       <Menu.Dropdown>
         <Menu.Item>
           <p
-          onClick={() => {
-            if (!form.values.text)
-              return showNotification({
-                title: "Error",
-                message: "Please enter post text",
-                color: "red",
-              });
-            savePostOrDraft("True");
-          }}
+            onClick={() => {
+              if (!form.values.text)
+                return showNotification({
+                  title: "Error",
+                  message: "Please enter post text",
+                  color: "red",
+                });
+              savePostOrDraft("True");
+            }}
             className="px-4 py-5 max-[390px]:px-2 flex gap-3 items-center cursor-pointer"
           >
             Share post
           </p>
         </Menu.Item>
         <Menu.Item>
-        <p
-          onClick={() => {
-            if (!form.values.text)
-              return showNotification({
-                title: "Error",
-                message: "Please enter post text",
-                color: "red",
-              });
-            savePostOrDraft("False");
-          }}
-            className="px-4 py-5 max-[390px]:px-2 flex gap-3 items-center cursor-pointer"
+          <p
+            onClick={() => {
+              if (!form.values.text)
+                return showNotification({
+                  title: "Error",
+                  message: "Please enter post text",
+                  color: "red",
+                });
+              savePostOrDraft("False");
+            }}
+            className="px-4 pb-5 max-[390px]:px-2 flex gap-3 items-center cursor-pointer"
           >
             Save to draft
           </p>
